22a69c7d1a911c0ab099ab2b51a677fb7ea96ea6
T.Shinohara <takuma1201@gmail.com>
2020-02-16 17:33:21 +0900

    自動構築環境の整備

--- pages/index.vue
+++ pages/index.vue
@@ -27,6 +27,10 @@
           <li v-for="item in articles" :key="item.event_id" target="_blank">
             <a :href="item.link[0]" target="_blank">{{ item.title[0] }}</a>
           </li>
+              <!-- link: [ 'https://blog.428lab.net/entry/2020/01/28/230000' ],
+              description: []
+              pubDate: [ 'Tue, 28 Jan 2020 23:00:00 +0900' ],
+              category: [ '機械学習', '自作PC', 'ハードウェア', '料理' ], -->
         </ul>
       </section>
       <section class="col-lg-6 ">
@@ -35,6 +39,29 @@
           <li v-for="item in events" :key="item.event_id" target="_blank">
             <a :href="item.event_url" target="_blank">{{ item.title }}</a>
           </li>
+          <!-- { event_id: 165655,
+            title: '自作キーボード 基板設計ハンズオン 4回目',
+            catch: '初心者向けのハンズオンとなります。基本は一人一回',
+            description: ''
+            event_url: 'https://428lab.connpass.com/event/165655/',
+            started_at: '2020-03-21T13:00:00+09:00',
+            ended_at: '2020-03-21T18:00:00+09:00',
+            limit: 6,
+            hash_tag: '自作キーボード設計',
+            event_type: 'participation',
+            accepted: 0,
+            waiting: 0,
+            updated_at: '2020-02-15T23:43:57+09:00',
+            owner_id: 307185,
+            owner_nickname: 'SHINOHARATTT',
+            owner_display_name: 'Shino3',
+            place: '全力機械（株） 四谷ラボ',
+            address: '東京都新宿区四谷２丁目８−８ 第５三和ビル 802号室',
+            lat: '35.686971700000',
+            lon: '139.727028400000',
+            series:
+            { id: 9445, title: '四谷ラボ', url: 'https://428lab.connpass.com/' }
+          } -->
         </ul>
       </section>
       <section class="col-12">
@@ -73,17 +100,6 @@ export default {
       "https://connpass.com/api/v1/event/?series_id=9445&order=2&count=10"
     );
     const blogResponse = await axios.get("https://blog.428lab.net/rss");
-    // axios.get(`RSSのURL`)
-    //     .then((res) => {
-    //       var parseString = require('xml2js').parseString
-    //       var xml = res.data
-    //       parseString(xml, (message, xmlres) => {
-    //         callback(null, {data: xmlres.rss.channel[0].item})
-    //       })
-    //     })
-    //     .catch((e) => {
-    //       callback({ statusCode: 404, message: 'ページが見つかりません' })
-    //     })
     let blogitems = {
     }
     xml2js.parseString(blogResponse.data, (message, xmlres) => {
