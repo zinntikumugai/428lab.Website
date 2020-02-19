5d3161a0a0b2313d3686b8732041e2bcc4f70268
zinntikumugai <uesitananame55@gmail.com>
2020-02-19 22:30:08 +0900

    この一つのオプションで良かったのか

--- deploy_develop.sh
+++ deploy_develop.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 yarn global add firebase-tools
-yarn install
-cd functions
-yarn install
-cd ..
-yarn build
-pwd
-ls
 firebase deploy --project=develop --token $FIREBASE_CI --only hosting,functions
