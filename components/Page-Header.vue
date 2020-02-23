f4a0a60a0226ffd230deb423dfa9f1434b130cd8
T.Shinohara <takuma1201@gmail.com>
2020-02-23 17:06:15 +0900

    ラボアイコンを追加して設定

--- components/Page-Header.vue
+++ components/Page-Header.vue
@@ -5,7 +5,7 @@
       <div
         class="d-md-flex align-items-end justify-content-between py-3 container px-0"
       >
-        <h1>四谷ラボ</h1>
+        <h1><img src="lab_icon.svg" alt="" height="64px"> 四谷ラボ</h1>
         <h2 class="h5 text-right">スポンサー：全力機械株式会社</h2>
       </div>
     </div>
