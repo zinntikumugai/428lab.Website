1b41f75aca1fa5428261fcc00e767ff069bf1d7e
zinntikumugai <uesitananame55@gmail.com>
2020-03-04 03:22:53 +0900

    fix deploy (ci)

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,10 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-# exist firebase
-if [ -n type "firebase" > /dev/null 2>&1 ]; then
-  yarn global add firebase-tools
-fi
-
 PROJECT=$1
 FIREBASE_ARG=" --project ${PROJECT}"
 if [ -n "$FIREBASE_CI" ]; then
+  yarn global add firebase-tools # HACK: Need only CI build?
   FIREBASE_ARG="${FIREBASE_ARG} --token ${FIREBASE_CI}"
 fi
 PROJECT_ID=$(firebase use ${FIREBASE_ARG})
