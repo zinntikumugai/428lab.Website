acb433743cec362bc179fd8c2e4c78d15f48aa7e
zinntikumugai <uesitananame55@gmail.com>
2020-03-04 03:04:18 +0900

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+
 if [ $# -ne 1 ]; then
   echo "引数エラー"
-  exit
+  exit 1
 fi
+
+# exist firebase
+if [ -n type "firebase" > /dev/null 2>&1 ]; then
+  yarn global add firebase-tools
+fi
+
 PROJECT=$1
-yarn global add firebase-tools
-cross-env BASE_URL=https:\/\/`firebase use --project $PROJECT --token $FIREBASE_CI | grep $PROJECT`.web.app yarn build
-firebase deploy --project=$PROJECT --token $FIREBASE_CI --only hosting,functions
+FIREBASE_ARG=" --project ${PROJECT}"
+if [ -n "$FIREBASE_CI" ]; then
+  FIREBASE_ARG="${FIREBASE_ARG} --token ${FIREBASE_CI}"
+fi
+PROJECT_ID=$(firebase use ${FIREBASE_ARG})
+
+BASE_URL="https://${PROJECT_ID}.web.app"
+yarn build
+firebase deploy ${FIREBASE_ARG} --only hosting,functions
